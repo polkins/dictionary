c2eb0ffbbfc1ecbb9f167461acb8569266394ad8
gordeeva-mv <mgordeeva@jtconsulting.ru>
2021-12-14 12:29:05 +0300

    change tests account

--- dictionary-impl/src/test/java/com/example/dictionary/DictionaryImplApplicationTests.java
+++ dictionary-impl/src/test/java/com/example/dictionary/DictionaryImplApplicationTests.java
@@ -4,6 +4,7 @@ import com.example.dictionary.api.dto.DictionaryAccountDto;
 import com.example.dictionary.api.dto.DictionaryBankDto;
 import com.example.dictionary.api.dto.StatusAccountDto;
 import com.example.dictionary.common.AbstractIntegrationTest;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
@@ -21,12 +22,8 @@ public class DictionaryImplApplicationTests extends AbstractIntegrationTest {
     protected TestRestTemplate restTemplate;
 
     @Test
-    public void createAndGetEntity() {
-        DictionaryBankDto dictionaryBankDto = new DictionaryBankDto()
-                .setAddress("Россия г.Рязань, ул.Букашкина 7")
-                .setBic("044525733")
-                .setName("Омега Банк");
-
+    public void create_bank_returnCorrectId() {
+        DictionaryBankDto dictionaryBankDto = getBankDto();
         HttpEntity<?> request = new HttpEntity<>(dictionaryBankDto, getHeaders());
 
         ResponseEntity<Long> createdBank = restTemplate.exchange(
@@ -39,7 +36,12 @@ public class DictionaryImplApplicationTests extends AbstractIntegrationTest {
         assertThat(createdBank.getBody()).isNotNull();
         Long id = createdBank.getBody();
         assertThat(id).isNotNull();
+        assertThat(id).isEqualTo(4);
+    }
 
+    @Test
+    public void getBank_bankId_returnCorrectBank() {
+        DictionaryBankDto dictionaryBankDto = getBankDto();
         HttpEntity<?> request2 = new HttpEntity<>(getHeaders());
 
         var selectedBank = restTemplate.exchange(
@@ -47,7 +49,7 @@ public class DictionaryImplApplicationTests extends AbstractIntegrationTest {
                 HttpMethod.GET,
                 request2,
                 DICTIONARY_BANK_TYPE_REFERENCE,
-                id
+                4
         );
 
         DictionaryBankDto bankBody = selectedBank.getBody();
@@ -58,25 +60,8 @@ public class DictionaryImplApplicationTests extends AbstractIntegrationTest {
     }
 
     @Test
-    public void createAndGetAccount() {
-        HttpEntity<?> requestForBank = new HttpEntity<>(getHeaders());
-
-        var selectedBank = restTemplate.exchange(
-                API_PREFIX + DICTIONARY + "/{id}",
-                HttpMethod.GET,
-                requestForBank,
-                DICTIONARY_BANK_TYPE_REFERENCE,
-                1
-        );
-
-        DictionaryBankDto bankDto = selectedBank.getBody();
-
-        DictionaryAccountDto dictionaryAccountDto = new DictionaryAccountDto()
-                .setAccountNumber(1234567891234567891L)
-                .setClientId(123L)
-                .setAccountStatus(StatusAccountDto.ACTIVE)
-                .setBank(bankDto);
-
+    public void create_account_returnCorrectId() {
+        DictionaryAccountDto dictionaryAccountDto = getDictionaryAccountDto();
         HttpEntity<?> request = new HttpEntity<>(dictionaryAccountDto, getHeaders());
 
         ResponseEntity<Long> createdAccount = restTemplate.exchange(
@@ -89,7 +74,11 @@ public class DictionaryImplApplicationTests extends AbstractIntegrationTest {
         assertThat(createdAccount.getBody()).isNotNull();
         Long id = createdAccount.getBody();
         assertThat(id).isNotNull();
+        assertThat(id).isEqualTo(5);
+    }
 
+    @Test
+    public void getAccount_accountId_returnAccount() {
         HttpEntity<?> request2 = new HttpEntity<>(getHeaders());
 
         var selectedAccount = restTemplate.exchange(
@@ -97,11 +86,13 @@ public class DictionaryImplApplicationTests extends AbstractIntegrationTest {
                 HttpMethod.GET,
                 request2,
                 DICTIONARY_ACCOUNT_TYPE_REFERENCE,
-                id
+                5
         );
 
+        DictionaryBankDto bankDto = getDictionaryBankDto();
         DictionaryAccountDto accountDto = selectedAccount.getBody();
         assertThat(accountDto).isNotNull();
+        DictionaryAccountDto dictionaryAccountDto = getDictionaryAccountDto();
         assertThat(accountDto.getAccountStatus()).isEqualTo(dictionaryAccountDto.getAccountStatus());
         assertThat(accountDto.getAccountNumber()).isEqualTo(dictionaryAccountDto.getAccountNumber());
         assertThat(accountDto.getClientId()).isEqualTo(dictionaryAccountDto.getClientId());
@@ -109,5 +100,35 @@ public class DictionaryImplApplicationTests extends AbstractIntegrationTest {
         assertThat(accountDto.getBank().getName()).isEqualTo(bankDto.getName());
         assertThat(accountDto.getBank().getAddress()).isEqualTo(bankDto.getAddress());
         assertThat(accountDto.getBank().getBic()).isEqualTo(bankDto.getBic());
+    }
+
+    @Nullable
+    private DictionaryBankDto getDictionaryBankDto() {
+        HttpEntity<?> requestForBank = new HttpEntity<>(getHeaders());
+        var selectedBank = restTemplate.exchange(
+                API_PREFIX + DICTIONARY + "/{id}",
+                HttpMethod.GET,
+                requestForBank,
+                DICTIONARY_BANK_TYPE_REFERENCE,
+                1
+        );
+        DictionaryBankDto bankDto = selectedBank.getBody();
+        return bankDto;
+    }
+
+    private DictionaryAccountDto getDictionaryAccountDto() {
+        DictionaryBankDto bankDto = getDictionaryBankDto();
+        return new DictionaryAccountDto()
+                .setAccountNumber(1234567891234567891L)
+                .setClientId(123L)
+                .setAccountStatus(StatusAccountDto.ACTIVE)
+                .setBank(bankDto);
+    }
+
+    private DictionaryBankDto getBankDto() {
+        return new DictionaryBankDto()
+                .setAddress("Россия г.Рязань, ул.Букашкина 7")
+                .setBic("044525733")
+                .setName("Омега Банк");
     }
 }
